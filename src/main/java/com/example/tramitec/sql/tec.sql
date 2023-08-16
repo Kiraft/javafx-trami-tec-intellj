2b0b0e895999276c0bbc168e63514e465beecad0
Jonathan <joniosva@gmail.com>
2023-08-16 15:53:09 -0600

    Agregada tabla 'Carreras' a la base de datos y establecida clave foránea en tabla 'Alumnos' para referenciar la nueva tabla en lugar de utilizar solo una columna VARCHAR para la carrera.

--- src/main/java/com/example/tramitec/sql/tec.sql
+++ src/main/java/com/example/tramitec/sql/tec.sql
@@ -5,19 +5,26 @@ CREATE DATABASE tramites_tecnm;
 
 USE tramites_tecnm;
 
+CREATE TABLE carreras (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    carrera VARCHAR(255) NOT NULL
+);
 CREATE TABLE alumnos (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    carrera_id INT UNSIGNED NOT NULL,
     nombres VARCHAR(255) NOT NULL,
     numero_control VARCHAR(255) NOT NULL UNIQUE,
     correo VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    carrera VARCHAR(255) NOT NULL
+
+    FOREIGN KEY (carrera_id) REFERENCES carreras(id)
 );
 
 CREATE TABLE tipos_archivo (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     nombre_archivo VARCHAR(255)  NOT NULL
 );
+
 
 CREATE TABLE archivos (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -31,34 +38,36 @@ CREATE TABLE archivos (
     FOREIGN KEY (tipos_archivo_id) REFERENCES tipos_archivo(id)
 );
 
-INSERT INTO alumnos (nombres, numero_control, correo, password, carrera) VALUES 
+INSERT INTO carreras (carrera) VALUES ("Ing. Sistemas Computacionales"), ("Ing. Mecatronica"), ("Contador Publico"), ("Ing Industrial"), ("Ing. Electronica");
+
+INSERT INTO alumnos (carrera_id, nombres, numero_control, correo, password) VALUES 
     (
-        "Jonathan Osvaldo",  
+        1,  
+        "Jonathan Osvaldo",
         "21680045", 
         "21680045@cuautla.tecnm.mx", 
-        "lolxx4321", 
-        "Sistemas Computacionales"
+        "lolxx4321" 
     ),
     (
-        "admin", 
+        1,
+        "admin",
         "admin",
         "admin@correo.com", 
-        "admin", 
-        "Sistemas Computacionales"
+        "admin" 
     ),
     (
+        2,
         "Mayra Abigail", 
         "21680175",
         "21680175@cuautla.tecnm.mx", 
-        "aby4321", 
-        "Sistemas Computacionales"
+        "aby4321" 
     ),
     (
+        3,
         "Said Alberto", 
         "20680183", 
         "20680183@cuautla.tecnm.mx", 
-        "zaidzaid123", 
-        "Sistemas Computacionales"
+        "zaidzaid123"
     );
 
 INSERT INTO tipos_archivo (nombre_archivo) VALUES 
@@ -86,4 +95,3 @@ INSERT INTO tipos_archivo (nombre_archivo) VALUES
     (
         "reporte final de actividad"
     );
-
